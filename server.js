8e7a16d183dd1dabf8b3f3a1a5f17e26fd610837
an nguyen <hello.an.ng@gmail.com>
2017-01-13 15:08:05 -0800

    saves exercises

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var app = express();
 app.use( express.static(__dirname + '/public' ));
 app.set('view engine', 'ejs'); // res.render(file) => '/views/file'
 
+var list = ['Mulder', 'Scully'];
+// define a route that returns JSON of the list info
+// GET 'list.json' => ['Mulder', 'Scully'];
+app.get('/list.json', function(req, res) {
+  res.json({ data: {list:list} });
+});
+
+app.get('/list', function(req, res){
+  var locals = {
+    message: "Hello from Views",
+    list: list
+  };
+  res.render('list', locals);
+});
 
 var port = 3000;
 app.listen(port, function(){
